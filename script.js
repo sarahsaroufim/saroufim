e1de17804192d41d27c3ac09a1ac0423dfccff8f
Sarah Saroufim <143719748+sarahsaroufim@users.noreply.github.com>
2024-05-21 15:15:27 -0400

    bones

---
+++ script.js
@@ -0,0 +1,9 @@
+function showImage(imageId) {
+    var image = document.getElementById(imageId);
+    image.style.opacity = "1";
+}
+
+function hideImage(imageId) {
+    var image = document.getElementById(imageId);
+    image.style.opacity = "0";
+}
