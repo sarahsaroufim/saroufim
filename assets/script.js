ac9c695a0f3bcbe27ef3cbfaff42c6c8cd1d547a
Sarah Saroufim <143719748+sarahsaroufim@users.noreply.github.com>
2024-05-22 15:35:48 -0400

    sentence cycle

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,5 @@
+// show & hide images on hover
+
 function showImage(imageId) {
     var image = document.getElementById(imageId);
     image.style.opacity = "1";
@@ -7,3 +9,47 @@ function hideImage(imageId) {
     var image = document.getElementById(imageId);
     image.style.opacity = "0";
 }
+
+
+// cycle sentences
+
+const sentences = [
+    "where do i start?",
+    "fix it before you break it",
+    "i need a place to put all of these thoughts",
+    "i could tell you, but i'd rather show you",
+    "immediately yes",
+    "it's all about balance, and how to tip it",
+    "i'm willing to wait",
+    "your right-hand woman",
+    "take me to your leader",
+    "there's something i have to say",
+    "it was all worth it in the end",
+    "même un artichaut a du cœur",
+    "i'm never shy, except for today",
+    "rise and shine it's midnight!",
+    "we'd make a great team you and i",
+    "my parents are so proud of me",
+    "i had a vision",
+    "simply overthinking",
+    "in the business of alchemy",
+    "i make the best of things",
+  ];
+  
+  let currentSentenceIndex = 0;
+  const sentenceElement = document.getElementById('sentence');
+  
+  function showNextSentence() {
+    sentenceElement.classList.remove('show');
+    
+    setTimeout(() => {
+      currentSentenceIndex = (currentSentenceIndex + 1) % sentences.length;
+      sentenceElement.textContent = sentences[currentSentenceIndex];
+      sentenceElement.classList.add('show');
+    }, 1000);
+  }
+  
+  sentenceElement.textContent = sentences[currentSentenceIndex];
+  sentenceElement.classList.add('show');
+  setInterval(showNextSentence, 3000); // interval duration
+  
